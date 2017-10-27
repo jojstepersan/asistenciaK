0986f2b67b86da926c30f949fe33f9a5c427dc68
Valentina\Valentina <vale.lindarte@gmail.com>
2017-10-27 15:12:42 -0500

    Trying login by email with firebase :/

--- TakeAssistance/app/src/main/java/co/edu/konradlorenz/takeassistance/activities/LoginActivity.java
+++ TakeAssistance/app/src/main/java/co/edu/konradlorenz/takeassistance/activities/LoginActivity.java
@@ -1,53 +1,19 @@
 package co.edu.konradlorenz.takeassistance.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import co.edu.konradlorenz.takeassistance.R;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity {
 
-
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
-    private  Button buttonLogin;
+    private Button buttonLogin;
     private EditText editTextUser;
     private EditText editTextPassword;
 
@@ -57,7 +23,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // Set up the login form.
-        editTextUser = (EditText) findViewById(R.id.user);
+        editTextUser = (EditText) findViewById(R.id.userEmail);
         editTextPassword = (EditText) findViewById(R.id.password);
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener((OnClickListener) this);
@@ -65,15 +31,9 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void userLogin(){
-        String user = editTextUser.getText().toString().trim();
-        String password = editTextPassword.toString().trim();
-    }
+    private void userLogin(String userEmail, String pass) {
+      
 
-    public void onClick(View view){
-        if(view == buttonLogin){
-            userLogin();
-        }
     }
 }
 
